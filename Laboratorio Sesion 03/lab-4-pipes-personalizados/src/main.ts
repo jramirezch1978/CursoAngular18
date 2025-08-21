5610e59a6751e329ac348df7492f193d642eaaf2
Jhonny Ramirez Chiroque <jramirez@npssac.com.pe>
2025-08-21 18:37:39 -0500

    laboratorio sesion 03

--- Laboratorio Sesion 03/lab-4-pipes-personalizados/src/main.ts
+++ Laboratorio Sesion 03/lab-4-pipes-personalizados/src/main.ts
@@ -1,11 +1,19 @@
 import 'zone.js';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
+import { registerLocaleData } from '@angular/common';
+import { LOCALE_ID } from '@angular/core';
+import localeEs from '@angular/common/locales/es';
+
 import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routes';
 
+// Registrar locale español
+registerLocaleData(localeEs);
+
 bootstrapApplication(AppComponent, {
   providers: [
-    provideRouter(routes)
+    provideRouter(routes),
+    { provide: LOCALE_ID, useValue: 'es' }
   ]
 }).catch(err => console.error(err));
